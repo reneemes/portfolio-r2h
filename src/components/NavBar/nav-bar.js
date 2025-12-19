0c023605322b46ff049625a5ddbe704c95f0ba80
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2025-12-19 18:49:39 -0500

    Fix: mobile nav bar animation

--- src/components/NavBar/nav-bar.js
+++ src/components/NavBar/nav-bar.js
@@ -1,19 +1,18 @@
 import "./nav-bar.css";
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
-
   return (
     <div className="header">
-      <div className="header__mode-icons">
-        <FontAwesomeIcon icon={faMoon} className="header__mode-icons--moon" size="xl" style={{ color: "#000000" }} />
-        <FontAwesomeIcon icon={faSun} className="header__mode-icons--sun" size="xl" style={{ color: "#ffffff" }} />
+      <div className="header__initial">
+        <a className="header__initial--char">R</a>
       </div>
       <nav className="header__nav-bar">
+        {/* Desktop Nav */}
         <div className="header__nav-bar--desktop">
           <a href="/">About</a>
           <a href="/reflections">Reflections</a>
@@ -21,17 +20,27 @@ function NavBar() {
           <a href="/resume">Resume</a>
         </div>
 
-        {!isOpen ? 
-          <FontAwesomeIcon icon={faBars} size="xl" id="hamburger-icon" onClick={() => setIsOpen(true)}/>
+        {/* Hamburger Icon */}
+        <FontAwesomeIcon icon={faBars} size="2xl" id="hamburger-icon" onClick={() => setIsOpen(true)}/>
+        {/* Mobile Nav */}
+        <div className={`header__nav-bar--mobile ${isOpen ? "open" : ""}`}>
+          <FontAwesomeIcon icon={faXmark} size="2xl" id="close-btn" onClick={() => setIsOpen(false)}/>
+          <a href="/">About</a>
+          <a href="/reflections">Reflections</a>
+          <a href="/contact">Contact</a>
+          <a href="/resume">Resume</a>
+        </div>
+        {/* {!isOpen ? 
+          <FontAwesomeIcon icon={faBars} size="2xl" id="hamburger-icon" onClick={() => setIsOpen(true)}/>
           :
           <div className="header__nav-bar--mobile">
-            <FontAwesomeIcon icon={faXmark} size="xl" id="close-btn" onClick={() => setIsOpen(false)}/>
+            <FontAwesomeIcon icon={faXmark} size="2xl" id="close-btn" onClick={() => setIsOpen(false)}/>
             <a href="/">About</a>
             <a href="/reflections">Reflections</a>
             <a href="/contact">Contact</a>
             <a href="/resume">Resume</a>
           </div>
-        }
+        } */}
       </nav>
     </div>
   );
