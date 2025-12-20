7b91100f2b0d5a3eaa7008ccf5ea54b2c2cd2f33
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2025-12-19 19:31:37 -0500

    Feat: updated footer styling and added text

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,8 +4,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <a className="footer__icon" href="https://www.linkedin.com/in/reneemessersmith/"><img className="footer__icon--img" src="/assets/linkedin.png" alt="LinkedIn Icon"/></a>
-      <a className="footer__icon" href="https://github.com/reneemes"><img className="footer__icon--img" src="/assets/github.png" alt="GitHub Icon"/></a>
+      <p className="footer__text">Created by Renee Messersmith</p>
+      <div className="footer__links">
+        <a className="footer__links-icon" href="https://www.linkedin.com/in/reneemessersmith/">
+          <img className="footer__links-icon--img" src="/assets/linkedin.png" alt="LinkedIn Icon"/>
+        </a>
+        <a className="footer__links-icon" href="https://github.com/reneemes">
+          <img className="footer__links-icon--img" src="/assets/github.png" alt="GitHub Icon"/>
+        </a>
+      </div>
     </footer>
   )
 }
