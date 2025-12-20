beaedf040078350ef39c6e996cd5346763c5a6b1
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2025-12-19 20:39:27 -0500

    Fix: contacts page formatting

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,51 +3,66 @@ import "./contact.css";
 function Contact() {
 
   return (
-      <section className="contact-page hidden">
-      <div className="contact-container">
-        <h1>Contact Me</h1>
-        <form className="contact-form">
-          <section className="name">
-            <input 
-              type="text" 
-              name="name" 
-              id="name" 
-              placeholder="Name (required)" 
-              
-            />
-          </section>
-          <section className="email">
-            <input 
-              type="text" 
-              name="email" 
-              id="email" 
-              placeholder="Email (required)" 
-              
-            />
-          </section>
-          <section className="phone-number">
-            <input 
-              type="tel" 
-              name="phone-number" 
-              id="phone-number" 
-              placeholder="Phone Number" 
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            />
-          </section>
-          <section className="comments">
-            <textarea 
-              type="text" 
-              name="comments" 
-              id="comments" 
-              placeholder="Please type here... (required)" 
-              maxLength="500"
-              
-            ></textarea>
-          </section>
+      <section className="contact">
+      <div className="contact__container">
+        <h1 className="contact__title">Contact Me</h1>
 
-          <section className="submit-button">
-            <input type="submit" value="Submit" id="submit-button"/>
-          </section>
+        <form className="contact__form">
+
+          <div className="contact__field">
+            <label for="name">Name</label>
+            <input
+              className="contact__input"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name (required)"
+              required
+            />
+          </div>
+
+          <div className="contact__field">
+            <input
+              className="contact__input"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email (required)"
+              required
+            />
+          </div>
+
+          <div className="contact__field">
+            <input
+              className="contact__input"
+              type="tel"
+              name="phone"
+              id="phone"
+              placeholder="Phone Number"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              required
+            />
+          </div>
+
+          <div className="contact__field">
+            <textarea
+              className="contact__textarea"
+              name="comments"
+              id="comments"
+              placeholder="Please type here... (required)"
+              maxLength="500"
+              required
+            />
+          </div>
+
+          <div className="contact__actions">
+            <input
+              className="contact__submit"
+              type="submit"
+              value="Submit"
+            />
+          </div>
+
         </form>
       </div>
     </section>
