a24a2db6111e86d9f17b962cbba5e9926970743b
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2025-12-20 20:07:27 -0500

    Feat: react pdf package working a sized for mobile

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -1,16 +1,27 @@
 import "./resume.css";
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
 
 function Resume() {
   return (
     <section className="resume">
       <h2 className="resume__title">Resume</h2>
-
-      <iframe src="/assets/resume.pdf" className="resume__container" allowFullScreen="true">
-        <p>
-          Your browser does not support PDFs.
-          <a href="/assets/resume.pdf" target="_blank">Download the PDF</a> instead.
-        </p>
-      </iframe>
+      <a href="/assets/resume.pdf" target="_blank" className="resume__link">
+        <Document 
+          file="/assets/resume.pdf"
+          onLoadError={console.error}
+        >
+          <Page pageNumber={1} width={350}/>
+        </Document>
+      </a>
     </section>
   )
 }
